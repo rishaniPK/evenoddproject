3788eceb70d93bddf8a6ad0195bb389cd5d85d97
Admin <Admin@DESKTOP-89NTJEN>
2019-05-21 10:20:07 +1200

    code

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenoddproject
+{
+    class Program
+    {
+        // This is a program to find a number weather odd or even number..!
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("This is an Even Number");
+
+            }
+            else
+            {
+                Console.WriteLine("This is an Odd Number");
+            }
+        }
+    }
+}
